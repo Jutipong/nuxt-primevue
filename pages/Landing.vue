baeeea73bb70426f1eb3027c9ec1b1b0d4241745
Jutipong <techcom.ec20@gmail.com>
2024-05-04 00:48:24 +0700

    chore: Update landing page layout and remove unused components

--- pages/Landing.vue
+++ pages/Landing.vue
@@ -421,23 +421,23 @@ const logoUrl = computed(() => {
 
                             <div class="col-12 md:col-3 mt-4 md:mt-0">
                                 <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Community</h4>
-                            <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Discord</a>
-                            <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Events<img
-                                    src="/demo/images/landing/new-badge.svg" class="ml-2" /></a>
-                            <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">FAQ</a>
-                            <a class="line-height-3 text-xl block cursor-pointer text-700">Blog</a>
-                        </div>
+                                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Discord</a>
+                                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Events<img
+                                        src="/demo/images/landing/new-badge.svg" class="ml-2" /></a>
+                                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">FAQ</a>
+                                <a class="line-height-3 text-xl block cursor-pointer text-700">Blog</a>
+                            </div>
 
-                        <div class="col-12 md:col-3 mt-4 md:mt-0">
-                            <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Legal</h4>
-                            <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Brand Policy</a>
-                            <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Privacy Policy</a>
-                            <a class="line-height-3 text-xl block cursor-pointer text-700">Terms of Service</a>
+                            <div class="col-12 md:col-3 mt-4 md:mt-0">
+                                <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Legal</h4>
+                                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Brand Policy</a>
+                                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Privacy Policy</a>
+                                <a class="line-height-3 text-xl block cursor-pointer text-700">Terms of Service</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<AppConfig simple /></template>
+</template>
