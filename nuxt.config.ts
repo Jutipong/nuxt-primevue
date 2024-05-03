3274f000f081ace2e93898456205ed0fcda7a74d
Jutipong <techcom.ec20@gmail.com>
2024-05-04 00:33:28 +0700

    chore: Update theme CSS to use aura-light-blue instead of aura-light-green

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
     head: {
@@ -8,11 +7,14 @@ export default defineNuxtConfig({
           id: 'theme-css',
           rel: 'stylesheet',
           type: 'text/css',
-          href: '/themes/aura-light-green/theme.css'
+          href: '/themes/aura-light-blue/theme.css'
         }
       ]
     }
   },
+  // typescript: {
+  //   typeCheck: true
+  // },
   devtools: { enabled: true },
   modules: ['nuxt-primevue'],
   primevue: {
@@ -21,21 +23,5 @@ export default defineNuxtConfig({
       exclude: ['Editor']
     }
   },
-  // script: [
-  //   {
-  //     strategy: 'lazyOnload',
-  //     src: 'https://www.googletagmanager.com/gtag/js?id=UA-93461466-1'
-  //   },
-  //   {
-  //     id: 'ga-analytics',
-  //     strategy: 'lazyOnload',
-  //     children: `
-  //               window.dataLayer = window.dataLayer || [];
-  //               function gtag(){dataLayer.push(arguments);}
-  //               gtag('js', new Date());
-  //               gtag('config', 'UA-93461466-1');
-  //           `
-  //   }
-  // ],
   css: ['primeicons/primeicons.css', 'primeflex/primeflex.scss', 'primevue/resources/primevue.min.css', '@/assets/styles.scss']
 })
