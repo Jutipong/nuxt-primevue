eef27ae532c405e68fecf449847e27579a7758c0
Jutipong <techcom.ec20@gmail.com>
2024-05-04 22:03:32 +0700

    refactor: Update layout styles, remove unused components, and configure VS Code settings

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+        stylistic: {
+      indent: 2,
+      overrides: {
+        // 'no-console': 'off'
+        // 'no-debugger': false,
+        // 'valid-v-slot': 1,
+      },
+    },
+    rules: {
+      'vue/valid-v-slot': ['error', { allowModifiers: true }],
+    },
+    ignores: [
+      '**/fixtures',
+    ],
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
+  },
+)
