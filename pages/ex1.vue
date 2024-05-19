531b2b8f9348d0d15d5148b4b01cf6bb4074b931
Jutipong <techcom.ec20@gmail.com>
2024-05-19 19:11:57 +0700

    refactor: Update form layout and add search functionality

--- pages/ex1.vue
+++ pages/ex1.vue
@@ -1,5 +1,13 @@
 <script lang="ts" setup>
-const toggleable = ref(true)
+const toggleable = ref(false)
+const cities = ref([
+    { name: 'Active', code: true },
+    { name: 'InActive', code: false },
+])
+const state = ref({
+    name: null,
+    status: null,
+})
 
 function toggle() {
     toggleable.value = !toggleable.value
@@ -9,7 +17,7 @@ function toggle() {
 <template>
     <div class="grid">
         <div class="col-12">
-            <Panel toggleable :collapsed="toggleable">
+            <Panel toggleable :collapsed="toggleable" @toggle="toggle">
                 <template #header>
                     <div class="flex align-items-center gap-1">
                         <Button
@@ -27,18 +35,24 @@ function toggle() {
                     <div class="row mt-1">
                         <div class="col-12 md:col-6 lg:col-6">
                             <div class="grid p-fluid field">
-                                <label for="name3" class="col-12 mb-2 md:col-2">Name</label>
+                                <label for="name" class="col-12 mb-2 md:col-2">Name</label>
                                 <div class="col-12 md:col-10">
-                                    <InputText id="name3" type="text" />
+                                    <InputText id="name" v-model="state.name" type="search" />
                                 </div>
                             </div>
                         </div>
 
                         <div class="col-12 md:col-6 lg:col-6">
                             <div class="grid p-fluid field">
-                                <label for="email3" class="col-12 mb-2 md:col-2">Email</label>
+                                <label for="statue" class="col-12 mb-2 md:col-2">Statue</label>
                                 <div class="col-12 md:col-10">
-                                    <InputText id="email3" type="text" />
+                                    <Dropdown
+                                        v-model="state.status"
+                                        option-value="code"
+                                        :options="cities"
+                                        show-clear
+                                        option-label="name"
+                                    />
                                 </div>
                             </div>
                         </div>
